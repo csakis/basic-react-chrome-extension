ca708ebef3f60a751138d1230b17440da61edf24
Csaba Kiss <csakis@gmail.com>
2019-12-10 16:09:55 -0700

    ready to use basic app

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <p>Kukucs</p>
+        <a href="https://reactjs.org">Learn React</a>
+      </header>
+      <h1>Holly molly! Its working</h1>
+      <h2>It also reloads</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/static/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from '../App';
+
+function renderApp() {
+  render(<App />, document.getElementById('root'));
+}
+
+renderApp();
+module.hot.accept();
